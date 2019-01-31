631a2cabe358c7c20f3a4751434b2ffeb1d0c37c
Mauno Vähä <maunovaha@gmail.com>
2019-01-31 08:53:38 +0200

    Adds the gameplay states

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Data/State")]
+[CreateAssetMenu(menuName = "Game/State")]
 public class State : ScriptableObject
 {
     [TextArea(14, 10)]
